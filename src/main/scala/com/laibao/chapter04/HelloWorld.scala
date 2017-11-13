067ddd4acf7cd9fab98d6a6d6f408b83bef0b76e
jinge <jinge@wacai.com>
2017-11-13 13:49:37 +0800

    update HelloWorld class info

--- src/main/scala/com/laibao/chapter04/HelloWorld.scala
+++ src/main/scala/com/laibao/chapter04/HelloWorld.scala
@@ -3,5 +3,18 @@ package com.laibao.chapter04
 object HelloWorld {
   def main(args: Array[String]): Unit = {
       println("this is the hello world scala example")
+
+    val scalaCar:ScalaCar = new ScalaCar(2017,"jin ge","hang zhou")
+
+    println(scalaCar.year)
+    println()
+    println(scalaCar.brand)
+    println()
+    println(scalaCar.manufacturer)
+    println()
+    scalaCar.drive(1000)
+    println()
+    println(scalaCar.milesDriven())
+
   }
 }
