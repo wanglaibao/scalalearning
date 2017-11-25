83e2f442104c2d7a9f8bab21bd90193ba75b646f
jinge <jinge@wacai.com>
2017-11-25 08:28:39 +0800

    add helloInfo.scala file info

---
+++ src/main/scala/com/laibao/chapter08/HelloInfo.scala
@@ -0,0 +1,11 @@
+package com.laibao.chapter08
+
+/**
+  * @author laibao wang
+  */
+object HelloInfo {
+
+  def main(args: Array[String]): Unit = {
+      println("hello world!Scala, chapter08!")
+  }
+}
