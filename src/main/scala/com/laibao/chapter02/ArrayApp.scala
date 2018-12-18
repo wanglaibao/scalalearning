cbe4b9d70e1d6077dd8bbf34846acd8d9b05b964
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-12-19 07:05:08 +0800

    add new file info

--- src/main/scala/com/laibao/chapter02/ArrayApp.scala
+++ src/main/scala/com/laibao/chapter02/ArrayApp.scala
@@ -6,5 +6,36 @@ package com.laibao.chapter02
 object ArrayApp {
   def main(args: Array[String]): Unit = {
     println("test")
+
+    val a = new Array[String](5)  //元素的默认值都是null
+
+
+    // 给数组的各个元素赋值
+    a(0) = "aaa"
+    a(1) = "bbb"
+    a(2) = "ccc"
+    a(3) = "ddd"
+    a(4) = "eee"
+
+    println(a.length)
+    //通过循环取数组中的数值
+    for (element <- a) {
+      println(element)
+    }
+    println()
+    println()
+    val b = Array("hadoop","spark","flink","storm")
+    for (element <- b) {
+      println(element)
+    }
+    b(2) = "BigData"
+    println()
+    println()
+    for (element <- b) {
+      println(element)
+    }
+
+    // Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: 4
+    //b(4)
   }
 }
--- src/main/scala/com/laibao/chapter02/ArrayExtendsApp.scala
+++ src/main/scala/com/laibao/chapter02/ArrayExtendsApp.scala
@@ -1,5 +1,23 @@
 package com.laibao.chapter02
 
 object ArrayExtendsApp extends App{
-  print("TEST TEST TEST")
+  println("TEST TEST TEST")
+  print()
+  val c = Array(1,2,3,4,5,6,7,8,9,10)
+  val len = c.length
+  println("len == " + len)
+  val sum = c.sum
+  println("sum == "+sum)
+  val maxValue = c.max
+  println("max value == "+maxValue)
+  val minValue = c.min
+  println("min value =="+minValue)
+  val str1 = c.mkString
+  println("str1 == "+str1)
+  val str2 = c.mkString(" ")
+  println("str2 =="+str2)
+  val str3 = c.mkString("||")
+  println("str3 =="+str3)
+  val str4 = c.mkString("<","|",">")
+  println("str4 =="+str4)
 }
