49a23c8db4b1cb3cc10851d4b511f4ae4abb8637
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-12-18 09:22:45 +0800

    add new file info

--- src/main/scala/com/laibao/chapter01/SimpleObjectApp.scala
+++ src/main/scala/com/laibao/chapter01/SimpleObjectApp.scala
@@ -1,15 +1,21 @@
 package com.laibao.chapter01
+
 object SimpleObjectApp {
 
   def main(args: Array[String]): Unit = {
     val person = new Person("金戈",100)
+    person.address = "杭州市"
     println(person.name + " and old is "+person.age)
     println(person.eat)
     person.watchFootBall("baba")
+    println(person.address)
+    person.printInfo()
   }
 }
 
 class Person ( val name:String,val age:Int){
+
+  var address:String = _
 
   def eat(): String = name + " is eating meal"
 
@@ -17,4 +23,10 @@ class Person ( val name:String,val age:Int){
     println(name + s" is watching foot ball match of $teamName")
   }
 
+  //  private [this] 这个只能在当前的Class内部使用
+  private [this] val gender = "male"
+
+  def printInfo(): Unit = {
+    println("the person's gender is " + gender)
+  }
 }
